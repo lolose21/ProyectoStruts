9076d05325f54f3e061519b19bb6b64f8d2d3b9d
Usuario <Usuario@DESKTOP-H22GIVL.technicolor.net>
2021-02-16 16:51:13 +0100

    mommit 00

---
+++ src/java/beans/BeanEmpleados.java
@@ -0,0 +1,69 @@
+package beans;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import models.Empleados;
+import repositories.RepositoryEmpleados;
+
+public class BeanEmpleados {
+
+    RepositoryEmpleados repo;
+
+    public BeanEmpleados() {
+        this.repo = new RepositoryEmpleados();
+    }
+
+    public String getEmpleadosOficio(String oficio) throws SQLException {
+        Empleados empleados
+                = this.repo.getbuscarOficio(oficio);
+        String html = "<table border='1'";
+
+        html += "<thead>";
+        html += "<tr>";
+        html += "<th>APELLIDO</th>";
+        html += "<th>OFICIO</th>";
+        html += "<th>SALARIO</th>";
+        html += "</tr>";
+        html += "</thead>";
+        html += "<tbody>";
+
+        html += "<tr>";
+        html += "<td>" + empleados.getApellido() + "</td>";
+        html += "<td>" + empleados.getOficio() + "</td>";
+        html += "<td>" + empleados.getSalario() + "</td>";
+        html += "</tr>";
+        html += "</tbody>";
+        html += "</table>";
+        return html;
+    }
+
+    public String getDetalles(int idempleado) throws SQLException {
+        Empleados empleado = this.repo.buscarEmpleado(idempleado);
+
+        String html = "<table border='1'";
+
+        html += "<thead>";
+        html += "<tr>";
+        html += "<th>EmpleadoNU</th>";
+        html += "<th>APELLIDO</th>";
+        html += "<th>OFICIO</th>";
+        html += "<th>SALARIO</th>";
+        html += "<th>DEPTNO</th>";
+        html += "</tr>";
+        html += "</thead>";
+        html += "<tbody>";
+
+        html += "<tr>";
+        html += "<td>" + empleado.getIdEmpleado() + "</td>";
+        html += "<td>" + empleado.getApellido() + "</td>";
+        html += "<td>" + empleado.getOficio() + "</td>";
+        html += "<td>" + empleado.getSalario() + "</td>";
+        html += "<td>" + empleado.getIddepartamento() + "</td>";
+        html += "<td>" + "<html:link action='/Action14Empleados' name='incremento'>" + "incremento" + "</html:link>" + "</td>";
+        html += "</tr>";
+        html += "</tbody>";
+        html += "</table>";
+        return html;
+    }
+
+}
