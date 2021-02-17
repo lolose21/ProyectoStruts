d0b65e93ceaf99e75c112498bebaa20dbea33a8e
Usuario <Usuario@DESKTOP-H22GIVL.technicolor.net>
2021-02-17 14:45:00 +0100

    mommit 00

---
+++ src/java/plugins/PlugInEnfermo.java
@@ -0,0 +1,41 @@
+package plugins;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import models.Enfermos;
+import org.apache.struts.action.ActionServlet;
+import org.apache.struts.action.PlugIn;
+import org.apache.struts.config.ModuleConfig;
+import repositories.RepositoryEnfermo;
+
+public class PlugInEnfermo implements PlugIn {
+
+    RepositoryEnfermo repo;
+
+    //creamos constuctor
+    public PlugInEnfermo() {
+        this.repo = new RepositoryEnfermo();
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void init(ActionServlet servlet, ModuleConfig config) throws ServletException {
+
+        try {
+            ArrayList<Enfermos> enfermo
+                    = this.repo.getEnfermos();
+            servlet.getServletContext().setAttribute("LISTAENFERMO", enfermo);
+        } catch (SQLException ex) {
+            System.out.println("Error SQL" + ex);
+
+        }
+
+    }
+}
